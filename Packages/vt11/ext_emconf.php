affefbdcaf6fdfdb82b227ea3762e77ac93ce08a
yxj0312 <yanxiajun@hotmail.com>
2022-01-18 21:56:50 +0100

    symlink vt11

---
+++ Packages/vt11/ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'VT11',
+    'description' => 'Sitepackage for TYPO3 11',
+    'category' => 'templates',
+    'author' => 'Wolfgang Wagner',
+    'author_email' => 'videotraining@wwagner.net',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
