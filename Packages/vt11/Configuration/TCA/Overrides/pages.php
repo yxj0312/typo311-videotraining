9e85f4802c20ee50b24b5441b11c9280a9ad1aa5
yxj0312 <yanxiajun@hotmail.com>
2022-01-30 22:46:49 +0100

    add Layout in TSConfig

--- Packages/vt11/Configuration/TCA/Overrides/pages.php
+++ Packages/vt11/Configuration/TCA/Overrides/pages.php
@@ -3,3 +3,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile('vt11','Configuration/TSconfig/BackendLayouts.tsconfig','Default Backendlayouts');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile('vt11','Configuration/TSconfig/Page_general.tsconfig','Default PageTSconfig');
